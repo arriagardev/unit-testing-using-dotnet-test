086c66b4d40c83dbceb59029ee1cd9a7beb17a3a
Humberto Arriaga <arriagar@gmail.com>
2021-01-21 00:50:31 -0600

    Added basic catalog functionality crud operations with EF and a code first

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -1,13 +1,10 @@
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MvcMovie
 {
@@ -24,6 +21,10 @@ namespace MvcMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
